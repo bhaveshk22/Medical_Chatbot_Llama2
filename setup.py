e766106b1766586f96d910ca1c1ab87442e8e45b
Bhavesh <bhaveshk2210@gmail.com>
2024-07-25 13:25:35 +0530

    setup file added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Medical Chatbot',
+    version= '0.0.0',
+    author= 'Bhavesh Kabdwal',
+    author_email= 'bhaveshk2210@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
